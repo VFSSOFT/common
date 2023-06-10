fbbae5ff3f8bc251f4c69207cade4db3a50363e1
martin <martin@9efe8e11-23cd-6f42-85db-bc41d7f0b8cf>
2023-06-10 13:53:41 +0000

    [Common] Add instance functions to MyHash
    git-svn-id: https://localhost:8443/svn/fs/common@353 9efe8e11-23cd-6f42-85db-bc41d7f0b8cf

--- MyHash.cpp
+++ MyHash.cpp
@@ -1,9 +1,65 @@
 #include "MyHash.h"
 
-#include "../common/MyCoreDef.h"
-#include <openssl/evp.h>
-#include <openssl/ossl_typ.h>
 
+
+MyHash::MyHash(): m_Ctx(NULL) {
+
+}
+MyHash::~MyHash() {
+    if (m_Ctx) {
+        EVP_MD_CTX_free(m_Ctx);
+    }
+    m_Ctx = NULL;
+}
+int MyHash::Init(int alg) {
+    int err = 0;
+    int success = 0; 
+
+    m_Ctx = EVP_MD_CTX_new();
+    if (m_Ctx == NULL)
+        return LastError(MY_ERR_OUT_OF_MEMORY, "Out of memory");
+
+    switch (alg) {
+    case MYHASH_ALG_SHA1:
+        if (!(success = EVP_DigestInit_ex(m_Ctx, EVP_sha1(), NULL))) {
+            return LastError(MY_ERR_CRYPTO_ERROR, "Init SHA1 failed");
+        }
+        break;
+
+    case MYHASH_ALG_SHA256:
+        if (!(success = EVP_DigestInit_ex(m_Ctx, EVP_sha256(), NULL))) {
+          return LastError(MY_ERR_CRYPTO_ERROR, "Init SHA256 failed");
+        }
+        break;
+
+    case MYHASH_ALG_MD5:
+          if (!(success = EVP_DigestInit_ex(m_Ctx, EVP_md5(), NULL))) {
+              return LastError(MY_ERR_CRYPTO_ERROR, "Init MD5 failed");
+          }
+          break;
+
+    default:
+        return LastError(MY_ERR_CRYPTO_INVALID_HASH_ALG, "Unsupported hash algorithm");
+    }
+    return 0;
+}
+int MyHash::Update(const char* b, int bLen) {
+    int success = EVP_DigestUpdate(m_Ctx, b, bLen);
+    if (!success) {
+        return LastError(MY_ERR_CRYPTO_ERROR, "Failed to calculate hash");
+    }
+    return 0;
+}
+int MyHash::Finish() {
+    unsigned int size = EVP_MD_CTX_get_size(m_Ctx);
+    m_HashValue.SetLength(size);
+
+    int success = EVP_DigestFinal(m_Ctx, (unsigned char*)m_HashValue.Deref(), &size);
+    if (!success) {
+        return LastError(MY_ERR_CRYPTO_ERROR, "Failed to calculate hash");
+    }
+    return 0;
+}
 
 int MyHash::CalcHash(int hashAlg, const char* data, int dataLen, unsigned char** out, unsigned int* outLen) {
   DECLARE_ERRCODE
--- MyHash.h
+++ MyHash.h
@@ -1,17 +1,33 @@
 #ifndef MY_HASH_H_
 #define MY_HASH_H_
 
+#include <openssl/evp.h>
+#include <openssl/ossl_typ.h>
+#include "MyStringA.h"
+#include "MyBuffer.h"
+
 #define MYHASH_ALG_SHA256 1
 #define MYHASH_ALG_SHA1   2
 #define MYHASH_ALG_MD5    3
 
 class MyHash {
 public:
-  MyHash() {}
-  virtual ~MyHash() {}
+  MyHash();
+  virtual ~MyHash();
+
+  int Init(int alg);
+  int Update(const char* b, int bLen);
+  int Finish();
+  MyBuffer* Result() { return &m_HashValue; }
 
   static int CalcHash(int hashAlg, const char* data, int dataLen, unsigned char** out, unsigned int* outLen);
 
+
+private:
+    EVP_MD_CTX* m_Ctx;
+    MyBuffer    m_HashValue;
+
+    MY_LAST_ERROR_DECL;
 };
 
 #endif // MY_HASH_H_

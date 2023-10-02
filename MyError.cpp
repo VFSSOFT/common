d30958ecf002f2d376851dce8008289f6fd594cc
martin <martin@9efe8e11-23cd-6f42-85db-bc41d7f0b8cf>
2023-10-02 15:31:01 +0000

    Add MyError.
    git-svn-id: https://localhost:8443/svn/fs/common@513 9efe8e11-23cd-6f42-85db-bc41d7f0b8cf

---
+++ MyError.cpp
@@ -0,0 +1,47 @@
+#ifndef _MY_ERROR_CPP_
+#define _MY_ERROR_CPP_
+
+#include "MyError.h"
+
+MyError::MyError() { Reset(); }
+MyError::MyError(int code, const char* msg) {
+    Set(code, msg);
+}
+MyError::MyError(int code, const wchar_t* msg) {
+    Set(code, msg);
+}
+MyError::MyError(const MyError& e) {
+    CopyFrom(e);
+}
+
+void MyError::Set(int code, const char* msg) {
+    m_Code = code;
+    SetMessage(msg);
+}
+void MyError::Set(int code, const wchar_t* msg) {
+    m_Code = code;
+    SetMessage(msg);
+}
+
+void MyError::Reset() {
+    m_Code = 0;
+    SetMessage("");
+}
+void MyError::CopyFrom(MyError* e) {
+    Set(e->m_Code, e->m_Message);
+}
+void MyError::CopyFrom(const MyError& e) {
+    Set(e.m_Code, e.m_Message);
+}
+
+void MyError::SetMessage(const char* msg) {
+    strcpy(m_Message, msg);
+}
+void MyError::SetMessage(const wchar_t* msg) {
+    MyStringA tmp;
+    tmp.SetUnicode(msg);
+    SetMessage(tmp.Deref());
+}
+
+
+#endif // _MY_ERROR_CPP_
---
+++ MyError.h
@@ -0,0 +1,33 @@
+#ifndef _MY_ERROR_H_
+#define _MY_ERROR_H_
+
+#include "MyStringA.h"
+
+class MyError {
+public:
+    MyError();
+    MyError(int code, const char* msg);
+    MyError(int code, const wchar_t* msg);
+    MyError(const MyError& e);
+
+    void operator=(const MyError& e) { CopyFrom(e); }
+
+    void Set(int code, const char* msg);
+    void Set(int code, const wchar_t* msg);
+
+    void Reset();
+    void CopyFrom(MyError* e);
+    void CopyFrom(const MyError& e);
+
+private:
+    void SetMessage(const char* msg);
+    void SetMessage(const wchar_t* msg);
+
+private:
+    int   m_Code;
+    char  m_Message[1024];
+};
+
+
+#endif // _MY_ERROR_H_
+
